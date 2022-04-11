f410a681c57318d477405057161d735351806347
ryanliu <xinrui.liu.1030@gmail.com>
2022-04-10 19:47:32 -0500

    Alpha 2

--- LazyBinarySearchTree.java
+++ LazyBinarySearchTree.java
@@ -12,38 +12,134 @@ public class LazyBinarySearchTree {
     }
 
     TreeNode root;
-    public LazyBinarySearchTree(){
-        root.key = 0;
-        root.leftChild = null;
-        root.rightChild = null;
-        root.deleted = false;
-    }
     private TreeNode find(int key){
         TreeNode temp = root;
+        if (root == null)
+            return null;
         while (temp.leftChild != null || temp.rightChild != null){
-            if (temp.key > key)
-                temp = temp.leftChild;
-
+            if (temp.key == key)
+                break;
+            else if (temp.key > key)
+                if (temp.leftChild != null)
+                    temp = temp.leftChild;
+                else
+                    break;
+            else if (temp.key < key)
+                if (temp.rightChild != null)
+                    temp = temp.rightChild;
+                else
+                    break;
         }
+        return temp;
     }
-
-    public boolean insert(int key){
+    public boolean checkRange(int key){
         if (key < 1 || key > 99)
-            throw new IllegalArgumentException("Key value needs to be between 1 and 99");
-        TreeNode temp = find(key, root);
-        if (temp.key == key)
             return false;
+        return true;
+    }
+    public boolean insert(int key){
+        if (!checkRange(key))
+            throw new IllegalArgumentException("Key value needs to be between 1 and 99");
+        TreeNode temp = find(key);
+        if (root == null){
+            root = temp;
+            return true;
+        }
+
+        if (temp.key == key){
+            if (temp.deleted)
+                temp.deleted = false;
+        }
         else if (temp.key > key){
             temp.leftChild = new TreeNode(key, null, null, false);
+            return true;
+        }
+        else {
+            temp.rightChild = new TreeNode(key, null, null, false);
+            return true;
         }
         return false;
     }
 
-    public int contains(int key){
+    public boolean delete(int key){
+        TreeNode temp = find(key);
+        if (temp.key == key){
+            temp.deleted = true;
+            return true;
+        }
+        return false;
+    }
 
-        return 0;
+    public boolean contains(int key){
+        if (checkRange(key))
+            throw new IllegalArgumentException("Key value needs to be between 1 and 99");
+        TreeNode temp = find(key);
+        if (temp != null && temp.key == key)
+            return true;
+        return false;
+    }
+
+    public int findMin(){
+        int min = root.key;
+        TreeNode temp = root;
+        while (temp != null){
+            if (temp != null && !temp.deleted)
+                min = Math.min(temp.key, min);
+            temp = temp.leftChild;
+        }
+
+        return min;
+    }
+
+    public int findMax(){
+        int max = root.key;
+        TreeNode temp = root;
+        while (temp != null){
+            if (temp != null && !temp.deleted)
+                max = Math.max(temp.key, max);
+            temp = temp.leftChild;
+        }
+
+        return max;
+    }
+
+    public int height(){
+        int height = traverseHeight(root, 0);
+        return height;
+    }
+
+    private int traverseHeight(TreeNode root, int count){
+        if (root != null)
+            return Math.max(traverseHeight(root.leftChild, count + 1),
+                    traverseHeight(root.rightChild, count + 1));
+
+        return count;
     }
 
 
+    public int size(){
+        int size = traverseSize(root, 0);
+        return size;
+    }
+    private int traverseSize(TreeNode root, int count){
+        if (root != null)
+            return traverseSize(root.leftChild, count + 1) + traverseSize(root.rightChild, count + 1);
+        return count;
+    }
+
+    public String toString(){
+        String result = traverseString(root, "");
+        return result;
+    }
+    private String traverseString(TreeNode root, String result){
+        if (root != null)
+            if (root.deleted)
+                return traverseString(root.leftChild, result +  "*" + root.key + " ") +
+                        traverseString(root.rightChild, result + "*" + root.key + " " );
+            else
+                return traverseString(root.leftChild, result + root.key + " ") +
+                        traverseString(root.rightChild, result + root.key + " ");
+        return result;
+    }
 
 }
---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main extends LazyBinarySearchTree {
+
+    public static void main(String[] args){
+        File file = new File("test.txt");
+        try{
+            Scanner sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        
+
+
+    }
+
+}
